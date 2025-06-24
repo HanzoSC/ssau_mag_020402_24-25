6a4ff222721ce3a1bdd46c308b77ce7591983a5f
Michael <108070636+HanzoSC@users.noreply.github.com>
2025-06-24 19:34:43 +0400

    Delete InfoSys/AngularLab directory
    Redirect to new repository

--- InfoSys/AngularLab/digital-department-application/src/app/pipes/birth-century.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BirthCenturyPipe } from './birth-century.pipe';
-
-describe('BirthCenturyPipe', () => {
-  it('create an instance', () => {
-    const pipe = new BirthCenturyPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
